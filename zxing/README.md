8e8fb12a34763c7c89a7eabea3960467b3232c8d
Deepika Tiwari <deepikatiwari92@gmail.com>
2020-10-30 22:49:39 +0100

    Update zxing results

--- zxing/README.md
+++ zxing/README.md
@@ -2,16 +2,16 @@
 
 ### Methods
 - Instrumented: 24
-- Invoked: 19
+- Invoked: 23
 
 ### Generated test cases
-- total: 1381
-- executed: 1381
-- passing: 1372 / 1380 (99.4%)
-- failing: 9 / 1380 (0.6%)
+- total: 1600
+- executed: 1600
+- passing: 1589 / 1600 (99.3%)
+- failing: 11 / 1600 (0.7%)
 
 ### Test assessment
-- pseudo-tested -> tested : 18 / 19
-- pseudo-tested -> partially tested : 1 / 19
-- pseudo-tested -> pseudo-tested : 0 / 19
+- pseudo-tested -> tested : 22 / 23
+- pseudo-tested -> partially tested : 1 / 23
+- pseudo-tested -> pseudo-tested : 0 / 23
 
--- zxing/execution/run.sh
+++ zxing/execution/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # define workload QR jpgs
-workloads=(./workload/qr_url.jpg ./workload/qr_email.jpg ./workload/qr_tel.jpg  ./workload/qr_geo.jpg ./workload/qr_sms.jpg ./workload/qr_contact_info_mecard.jpg ./workload/qr_contact_info_vcard.jpg ./workload/qr_contact_info_bizcard.jpg ./workload/qr_calendar.jpg )
+workloads_qr=(./workload/qr_codes/url.jpg ./workload/qr_codes/email.jpg ./workload/qr_codes/tel.jpg  ./workload/qr_codes/geo.jpg ./workload/qr_codes/sms.jpg ./workload/qr_codes/mecard.jpg ./workload/qr_codes/vcard.jpg ./workload/qr_codes/bizcard.jpg ./workload/qr_codes/calendar.jpg ./workload/qr_codes/wifi.jpg)
+workloads_barcode=(./workload/barcodes/code39.png ./workload/barcodes/rss.png ./workload/barcodes/ean13.png ./workload/barcodes/upc.png)
+
 glowroot_jar_loc="/home/user/glowroot/glowroot.jar"
 pankti_zxing_jar_loc="./pankti-zxing-3.4.1-jar-with-dependencies.jar"
 zxing_jar_loc="./javase-3.4.1-jar-with-dependencies.jar"
@@ -8,8 +10,15 @@ zxing_jar_loc="./javase-3.4.1-jar-with-dependencies.jar"
 # Generate QR JPGs in ./workload/
 java -javaagent:${glowroot_jar_loc} -jar ${pankti_zxing_jar_loc} 
 
-# execute ZXing for each workload
-for i in ${workloads[@]}
+# execute ZXing for each QR workload
+for i in ${workloads_qr[@]}
+do
+  echo "Parsing " ${i}
+  java -javaagent:${glowroot_jar_loc} -jar ${zxing_jar_loc} ${i}
+done
+
+# execute ZXing for each barcode workload
+for i in ${workloads_barcode[@]}
 do
   echo "Parsing " ${i}
   java -javaagent:${glowroot_jar_loc} -jar ${zxing_jar_loc} ${i}
